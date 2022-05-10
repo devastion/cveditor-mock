63ea60460dea94a145eb71d8a596380d021eddf0
Dimitar Banev <banev_dimitar@mail.com>
2022-05-10 11:18:25 +0300

    fix: userevent instead fireevent on the labels so we can have focus on inputs

--- src/__tests__/AddExperience.test.tsx
+++ src/__tests__/AddExperience.test.tsx
@@ -1,13 +1,41 @@
-import { render, fireEvent, screen } from "../utils/test-utils";
+import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "../utils/test-utils";
 import AddExperience from "../components/Experience/AddExperience";
 
 describe("Testing input focus", () => {
-  const { getByTestId } = render(<AddExperience />);
-  test("Title input", () => {
-    const titleInput = getByTestId("title-input");
-    const titleLabel = getByTestId("title-label");
-    fireEvent.click(titleLabel);
-    titleInput.focus();
-    expect(titleInput).toHaveFocus();
+  test("Title input", async () => {
+    render(<AddExperience />);
+    const titleInput = screen.getByTestId("title-input");
+    const titleLabel = screen.getByTestId("title-label");
+    expect(titleInput).not.toHaveFocus();
+    userEvent.click(titleLabel);
+    await waitFor(() => expect(titleInput).toHaveFocus());
+  });
+
+  test("Company input", async () => {
+    render(<AddExperience />);
+    const titleInput = screen.getByTestId("company-input");
+    const titleLabel = screen.getByTestId("company-label");
+    expect(titleInput).not.toHaveFocus();
+    userEvent.click(titleLabel);
+    await waitFor(() => expect(titleInput).toHaveFocus());
+  });
+
+  test("Date input", async () => {
+    render(<AddExperience />);
+    const titleInput = screen.getByTestId("date-input");
+    const titleLabel = screen.getByTestId("date-label");
+    expect(titleInput).not.toHaveFocus();
+    userEvent.click(titleLabel);
+    await waitFor(() => expect(titleInput).toHaveFocus());
+  });
+
+  test("City input", async () => {
+    render(<AddExperience />);
+    const titleInput = screen.getByTestId("city-input");
+    const titleLabel = screen.getByTestId("city-label");
+    expect(titleInput).not.toHaveFocus();
+    userEvent.click(titleLabel);
+    await waitFor(() => expect(titleInput).toHaveFocus());
   });
 });
