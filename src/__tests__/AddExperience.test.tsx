5d33f89565aae96869300aa24fe8131a6ad7b345
Dimitar Banev <banev_dimitar@mail.com>
2022-05-10 10:44:02 +0300

    fix: fixing test

--- src/__tests__/AddExperience.test.tsx
+++ src/__tests__/AddExperience.test.tsx
@@ -2,35 +2,12 @@ import { render, fireEvent, screen } from "../utils/test-utils";
 import AddExperience from "../components/Experience/AddExperience";
 
 describe("Testing input focus", () => {
+  const { getByTestId } = render(<AddExperience />);
   test("Title input", () => {
-    render(<AddExperience />);
-    const titleInput = screen.getByTestId("title-input");
-    const titleLabel = screen.getByTestId("title-label");
+    const titleInput = getByTestId("title-input");
+    const titleLabel = getByTestId("title-label");
     fireEvent.click(titleLabel);
-    expect(titleInput.focus());
-  });
-
-  test("Company input", () => {
-    render(<AddExperience />);
-    const titleInput = screen.getByTestId("company-input");
-    const titleLabel = screen.getByTestId("company-label");
-    fireEvent.click(titleLabel);
-    expect(titleInput.focus());
-  });
-
-  test("Date input", () => {
-    render(<AddExperience />);
-    const titleInput = screen.getByTestId("date-input");
-    const titleLabel = screen.getByTestId("date-label");
-    fireEvent.click(titleLabel);
-    expect(titleInput.focus());
-  });
-
-  test("City input", () => {
-    render(<AddExperience />);
-    const titleInput = screen.getByTestId("city-input");
-    const titleLabel = screen.getByTestId("city-label");
-    fireEvent.click(titleLabel);
-    expect(titleInput.focus());
+    titleInput.focus();
+    expect(titleInput).toHaveFocus();
   });
 });
